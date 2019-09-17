84ee6a5c936bef1841627e8f6a4c1d60a1d918af
chuck-durst <chuckdurst67@gmail.com>
2019-09-17 17:16:28 +0200

    [WIP] Trying to fix babel/webpack issues - build files with babel

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  presets: [[
+    '@babel/preset-env',
+    {
+      modules: 'commonjs',
+    },
+  ], '@babel/preset-react'],
+  plugins: [
+    'babel-plugin-transform-react-jsx',
+    'babel-plugin-transform-react-constant-elements',
+    'babel-plugin-transform-dev-warning',
+    'babel-plugin-optimize-clsx',
+    '@babel/plugin-transform-object-assign',
+    ['@babel/plugin-transform-runtime', { useESModules: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-object-rest-spread', { loose: true }],
+  ],
+};
