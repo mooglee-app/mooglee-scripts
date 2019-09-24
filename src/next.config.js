e16c3005616f549c82428cbe4bf0dfe0a7d3739e
chuck-durst <chuckdurst67@gmail.com>
2019-09-24 14:38:00 +0200

    Use the next-compose-plugins module in next.config.js

--- src/next.config.js
+++ src/next.config.js
@@ -2,22 +2,31 @@ const withSass      = require('@zeit/next-sass');
 const webpackConfig = require('./server/webpack.config');
 const withOffline   = require('next-offline');
 const workboxOpts   = require('./server/serviceWorker.config');
-const withTM = require('next-transpile-modules');
+const withTM        = require('next-transpile-modules');
+const withPlugins   = require('next-compose-plugins');
 
-module.exports      =withTM(/* withOffline(*/withSass({
-  transpileModules: ['@mooglee', '@mooglee/core'],
-  cssModules: true,
+
+module.exports = withPlugins([
+  [withSass, {
+    cssModules: true,
+    cssLoaderOptions: {
+      importLoaders: 1,
+      localIdentName: '[local]',
+    },
+  }],
+  [withOffline, {
+    workboxOpts,
+    dontAutoRegisterSw: true,
+    generateInDevMode: true,
+  }],
+  [withTM, {
+    transpileModules: ['@mooglee', '@mooglee/core'],
+  }],
+], {
   distDir: './build', // from client folder
-  workboxOpts,
-  dontAutoRegisterSw: true,
-  generateInDevMode: true,
   useFileSystemPublicRoutes: false,
-  cssLoaderOptions: {
-    importLoaders: 1,
-    localIdentName: '[local]',
-  },
 
   webpack: (config, { dev, isServer, buildId, config: { distDir } }) => {
     return webpackConfig(config, { isServer, buildId, distDir, dev });
   },
-}))/*)*/;
+});
