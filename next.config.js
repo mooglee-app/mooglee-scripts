914ee7d603651eea6ebc6003164c974e9e67680e
chuck-durst <hello@charlesdurst.fr>
2021-08-27 19:36:11 +0200

    release v1.10.0

--- next.config.js
+++ next.config.js
@@ -1,29 +1,56 @@
+const withSass      = require('@zeit/next-sass');
 const webpackConfig = require('./config/webpack.config');
 const workboxOpts   = require('./config/serviceWorker.config')
-const paths         = require('./lib/paths');
 //@remove-on-eject-begin
 const withTM        = require('next-transpile-modules');
 //@remove-on-eject-end
 const withOffline   = require('next-offline');
-
-const nextConfig = /*@add-on-eject-begin({@add-on-eject-end*/
+const regexEqual    = (x, y) => {
+  return (
+    x instanceof RegExp &&
+    y instanceof RegExp &&
+    x.source === y.source &&
+    x.global === y.global &&
+    x.ignoreCase === y.ignoreCase &&
+    x.multiline === y.multiline
+  );
+};
+const nextConfig = withSass(/*@add-on-eject-begin({@add-on-eject-end*/
   //@remove-on-eject-begin
   withTM(['@mooglee/core'])(
     {
       //@remove-on-eject-end
+      cssModules: true,
       distDir: './build', // from client folder
       dontAutoRegisterSw: true,
       generateInDevMode: true,
       useFileSystemPublicRoutes: false,
       workboxOpts,
-      sassOptions: {
-        includePaths: [paths.app + 'styles'],
+      cssLoaderOptions: {
+        importLoaders: 1,
+        localIdentName: '[local]',
       },
 
       webpack: (config, { dev, isServer, buildId, config: { distDir } }) => {
         return webpackConfig(config, { isServer, buildId, distDir, dev });
       },
-    })
+    }));
 
+// Fix a bug with transpile-modules
+if (typeof nextConfig.webpack === 'function') {
+  const prevWebpack  = nextConfig.webpack;
+  nextConfig.webpack = (config, options) => {
+    const nextCssLoaders = config.module.rules.find((rule) => typeof rule.oneOf === 'object');
+
+    if (nextCssLoaders) {
+      const nextCssLoader = nextCssLoaders.oneOf.find(
+        (rule) => rule.sideEffects === false && regexEqual(rule.test, /\.module\.css$/),
+      );
+
+      nextCssLoader.issuer.include = [];
+    }
+    return prevWebpack(config, options);
+  }
+}
 
 module.exports = process.env.NODE_ENV === 'production' ? withOffline(nextConfig) : nextConfig;
